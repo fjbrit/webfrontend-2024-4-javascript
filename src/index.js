cc08cfa6aa76bfba0ba4a50cebab995913d91d9c
Fabio <brito443@gmail.com>
2024-09-25 20:54:40 -0300

    Testando algumas alterações

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Importando a biblioteca readline-sync
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
 // Capturando o número de pessoas na mesa
 const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa: ');
@@ -8,12 +8,12 @@ const numeroPessoas = readline.questionInt('Digite o número de pessoas na mesa:
 const valorTotal = readline.questionFloat('Digite o valor total da conta: ');
 
 // Capturando o método de pagamento
-const metodoPagamento = readline.question('Qual é o método de pagamento (PIX, dinheiro ou cartão)? ');
+const metodoPagamento = readline.question("Qual é o método de pagamento (PIX, dinheiro ou cartão)? ");
 
 // Lógica para aplicar desconto para pagamento em PIX ou dinheiro
 let valorFinal = valorTotal;
 
-if (metodoPagamento.toLowerCase() === 'pix' || metodoPagamento.toLowerCase() === 'dinheiro') {
+if (metodoPagamento.toLowerCase() === "pix" || metodoPagamento.toLowerCase() === 'dinheiro') {
   const desconto = 0.1; // 10% de desconto
   valorFinal = valorTotal - (valorTotal * desconto);
   console.log(`Você recebeu um desconto de 10%. Valor final: R$ ${valorFinal.toFixed(2)}`);
